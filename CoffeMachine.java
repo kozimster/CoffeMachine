61d2dce5dda0a7f91156be71c5944c179a5c6f10
Marcin Kozak <kozimster@gmail.com>
2020-11-03 08:29:40 +0000

    first commit

---
+++ CoffeMachine.java
@@ -0,0 +1,24 @@
+/*JetBrains Academy Coffee Machine project
+ * Description
+
+Let's start with a program that makes you a coffee – virtual coffee, of course. But in this project, 
+you will implement functionality that simulates a real coffee machine. It can run out of milk, it can run out of coffee beans,
+ it can make different varieties of coffee, and it can take the money for making a coffee.
+
+The first version of the program just makes you a coffee. It prints to the standard output what it is doing as it makes a coffee.
+ * 
+ */
+public class CoffeMachine {
+
+	public static void main(String[] args) {
+		System.out.println("Startin to make coffee");
+		System.out.println("Grinding coffee beans");
+		System.out.println("Boiling water");
+		System.out.println("Mixing boiled water with crushed coffee beans");
+		System.out.println("Pouring coffee into the cup");
+		System.out.println("Pouring some milk into the cup");
+		System.out.println("Coffee is ready!");
+
+	}
+
+}
