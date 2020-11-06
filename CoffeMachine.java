b9a75d95d577dae5f78557a0a88eca54ae452910
Marcin Kozak <kozimster@gmail.com>
2020-11-06 05:44:20 +0000

    3 stage

--- CoffeMachine.java
+++ CoffeMachine.java
@@ -8,6 +8,7 @@ you will implement functionality that simulates a real coffee machine. It can ru
 The first version of the program just makes you a coffee. It prints to the standard output what it is doing as it makes a coffee.
  * Stage 1 - Write a program that prints the steps necessary to make coffee.
  * Stage 2 - Write a program that calculates the amount of ingredients needed to make a certain amount of coffee.
+ * Stage 3 - Write a program that calculates whether it will be able to make the required amount of coffee from the specified amount of ingredients.
  */
 import java.util.Scanner;
 public class CoffeMachine {
@@ -22,25 +23,33 @@ public class CoffeMachine {
 		System.out.println("Pouring some milk into the cup");
 		System.out.println("Coffee is ready!");
 
+		int waterNeed= 200;
+		int milkNeed = 50;
+		int coffeeNeed = 15;
+		int n =0;
+		
 		Scanner scan = new Scanner(System.in);
 		int cups;
 		int water;
 		int milk;
 		int coffeeBeans;
 		
+		System.out.println("Write how many ml of water the coffee machine has:");
+		water = scan.nextInt();
+		System.out.println("Write how many ml of milk the coffee machine has:");
+		milk = scan.nextInt();
+		System.out.println("Write how many grams of coffee beans the coffee machine has:");
+		coffeeBeans = scan.nextInt();
 		
 		System.out.println("Write how many cups of coffee you will need:");
 		cups = scan.nextInt();
 		scan.close();
 		
-		water = cups*200;
-		milk = cups*50;
-		coffeeBeans = cups* 15;
+		if (water < waterNeed && milk < milkNeed && coffeeBeans < coffeeNeed) {
+			
+		}
 		
-		System.out.println("For " + cups + " cups of coffee you will need:");
-		System.out.println(water + " ml of water");
-		System.out.println(milk + " ml of milk");
-		System.out.println(coffeeBeans + " g of coffee beans");
+		
 		
 	}
 
